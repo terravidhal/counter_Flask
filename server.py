429b5c8bcc8603861865637093f8a72893757200
terravidhal <infiniterraetincelle@gmail.com>
2023-09-03 08:17:28 +0100

    delete api_key

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request, session  # added session!
 
 app = Flask(__name__)
-app.secret_key = '7a63b7ac5f8a5640035460f78fc4f8e17dd303eb4b78eeb01c3cd6e53db3b43f' # python -c 'import secrets; print(secrets.token_hex())'
+app.secret_key = '' # python -c 'import secrets; print(secrets.token_hex())'
 
 @app.route("/") 
 def home():
